28aa015b17b7920658f80f9271da60cc406bce5c
teomoura <mourateogenes@gmail.com>
2021-01-11 23:16:07 -0300

    #14 Implementing local storage for saving and loading the nomination list

--- src/containers/DashboardContainer/__test__/index.test.js
+++ src/containers/DashboardContainer/__test__/index.test.js
@@ -34,6 +34,13 @@ test('clicking "search movie" submits request to external API', async () => {
 
 describe('Testing interactions on DashboardContainer', () => {
   const wrapper = mount(<MockTheme><DashboardContainer></DashboardContainer></MockTheme>)
+
+  test('sets state from local storage', () => {
+    const nominatedMovie = JSON.stringify({"Title":"Star Wars: Episode IV - A New Hope","Year":"1977","imdbID":"tt0076759","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"})
+    window.localStorage.setItem('@shoppies/nominationList', nominatedMovie)
+    const dashboard = mount(<DashboardContainer></DashboardContainer>)
+  })
+
   test('Searching a movie doesnt break page', () => {
     const searchBoxWrapper = wrapper.findWhere(node => node.is(SearchBox))
     const input = searchBoxWrapper.find('#outlined-search').last()
